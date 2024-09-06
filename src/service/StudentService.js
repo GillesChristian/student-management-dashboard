eb0de50edfb7f4f13522113c3faff0463de60595
Gilles Christian kamgaing <120981853+GillesChristian@users.noreply.github.com>
2024-09-06 15:55:33 +0100

    Update StudentService.js

--- src/service/StudentService.js
+++ src/service/StudentService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = 'http://127.0.0.1:8000/api/students';
 
-export const fetchStudents = () => axios.get(API_URL, { timeout: 1000, });
+export const fetchStudents = () => axios.get(API_URL);
 export const fetchStudentById = (id) => axios.get(`${API_URL}/${id}`);
 export const createStudent = (studentData) => axios.post(API_URL, studentData);
 export const updateStudent = (id, studentData) => axios.put(`${API_URL}/${id}`, studentData);
